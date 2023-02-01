8bea1799e642e3427b0ca61fffc6a4898b8bc7b1
Mike Robles <mikej.robles@gmail.com>
2023-02-01 18:27:59 -0500

    creates navbar functionality

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import Contact from './components/Pages/Contact'
 import Resume from './components/Pages/Resume'
 import ProjectsList from './components/Pages/ProjectsList'
 import Navbar from './components/NavBar/Navbar'
-// import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    // <Navbar />
-    <Routes>
-      <Route path='/' element={<Landing/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/resume' element={<Resume/>} />
-      <Route path='/projects' element={<ProjectsList/>} />
-
-    </Routes>
-  );
+    <>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/resume' element={<Resume/>} />
+          <Route path='/projects' element={<ProjectsList/>} />
+        </Routes>
+    </>
+  )
 }
 
 export default App;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from 'react-router-dom'
+import logo from '../../logo.svg'
+
 const Navbar = () => {
   return (
-    <h1>Links for Navbar here</h1>
+    <nav>
+      <NavLink to={'/'}>Michael Robles</NavLink>
+      <NavLink to={'/about'}>About</NavLink>
+      <NavLink to={'/contact'}>Contact</NavLink>
+      <NavLink to={'/resume'}>Resume</NavLink>
+      <NavLink to={'/projects'}>Projects</NavLink>
+    </nav>
   )
 }
 
