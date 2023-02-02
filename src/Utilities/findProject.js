473a6807cc48450ed500027662a955c70ea45518
Mike Robles <mikej.robles@gmail.com>
2023-02-02 08:50:23 -0500

    hypenateWords functionality done

---
+++ src/Utilities/findProject.js
@@ -0,0 +1,8 @@
+import { projects } from "../data/Projects"
+import {hyphenateWords} from './hyphenateWords'
+
+function findProject(string) {
+  projects.filter(project => )
+}
+
+export default findProject
