5b0431eb8ae4c09905dd0247228b839d45ff6116
Mike Robles <mikej.robles@gmail.com>
2023-03-14 21:58:35 -0400

    changes to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <ul>
-        <li><NavLink className={styles.link} to={'/'}>Mike</NavLink></li>
+        <li><NavLink className={styles.link} to={'/'}>MR</NavLink></li>
         <li><NavLink className={styles.link} to={'/about'}>About</NavLink></li>
-        <li><NavLink className={styles.link} to={'/contact'}>Contact</NavLink></li>
-        <li><NavLink className={styles.link} to={'/resume'}>Resume</NavLink></li>
         <li><NavLink className={styles.link} to={'/projects'}>Projects</NavLink></li>
+        <li><NavLink className={styles.link} to={'/resume'}>Resume</NavLink></li>
+        <li><NavLink className={styles.link} to={'/contact'}>Contact</NavLink></li>
       </ul>
     </nav>
   )
