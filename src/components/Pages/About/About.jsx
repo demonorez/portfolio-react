a19c54f4e80ee347b4d7b9a27cf43c3d8c6b890a
Mike Robles <mikej.robles@gmail.com>
2023-03-17 18:15:58 -0400

    Some about changes

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -6,8 +6,8 @@ function About() {
       </div>
       <div class='about'>
         <h1>Who am I?</h1>
-        <h2>I am a Software Engineer with a history of customer service. I spent many years in customer service and have developed a strong sense of empathy for others. I have a high degree of patience which have combined to make me an exceptional team player.</h2>
-        <h3>I love playing video games (I'm diamond in League of Legends), fantasy, sci-fi, nature, Porter Robinson, Madeon, anime, and am currently learning Japanese with Duolingo.</h3>
+        <h2>I am a Software Engineer with previous experience in customer service. Through several years in this space I have developed a strong sense of empathy for others. Exceptional team player with a high degree of patience.</h2>
+        <h2>Outside of the professional space, I love playing video games (I'm diamond in League of Legends), fantasy, sci-fi, nature, Porter Robinson, Madeon, anime, and am currently learning Japanese with Duolingo.</h2>
       </div>
     </>
   )
