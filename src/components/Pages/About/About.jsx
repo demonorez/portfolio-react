ca1c2792cf953d294fa7e826b14766db49d4f15b
Mike Robles <mikej.robles@gmail.com>
2023-03-29 16:09:53 -0400

    about changes

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -4,10 +4,12 @@ function About() {
       <div class='img'>
         <img src={'/Mike.jpg'} alt='Mike profile pic'></img>
       </div>
-      <div class='about'>
         <h1>Who am I?</h1>
+      <div class='about-left'>
         <h2>I am a Software Engineer with previous experience in customer service. Through several years in this space I have developed a strong sense of empathy for others. Exceptional team player with a high degree of patience.</h2>
+        <div class='about-right'>
         <h2>Outside of the professional space, I love playing video games (I'm diamond in League of Legends), fantasy, sci-fi, nature, Porter Robinson, Madeon, anime, and am currently learning Japanese with Duolingo.</h2>
+        </div>
       </div>
     </>
   )
